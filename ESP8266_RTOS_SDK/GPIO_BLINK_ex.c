8bfd207789b9cf37273c65155e0f0c5845a45276
dnjswp12 <49463724+dnjswp12@users.noreply.github.com>
2019-07-02 15:22:36 +0900

    Update GPIO_BLINK_ex.c

--- ESP8266_RTOS_SDK/GPIO_BLINK_ex.c
+++ ESP8266_RTOS_SDK/GPIO_BLINK_ex.c
@@ -43,29 +43,7 @@ static const char *TAG = "main";
 #define GPIO_OUTPUT_IO_0    0
 #define GPIO_OUTPUT_IO_1    4
 #define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_0) | (1ULL<<GPIO_OUTPUT_IO_1))
-//#define GPIO_INPUT_IO_0     4
-//#define GPIO_INPUT_IO_1     5
-//#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0) | (1ULL<<GPIO_INPUT_IO_1))
 
-//static xQueueHandle gpio_evt_queue = NULL;
-
-/*static void gpio_isr_handler(void *arg)
-{
-    uint32_t gpio_num = (uint32_t) arg;
-    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
-}*/
-
-/*static void gpio_task_example(void *arg)
-{
-    uint32_t io_num;
-    int i;
-
-    for (;;) {
-        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
-            ESP_LOGI(TAG, "GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
-        }
-    }
-}*/
 
 void app_main(void)
 {
@@ -82,36 +60,7 @@ void app_main(void)
     io_conf.pull_up_en = 0;
     //configure GPIO with the given settings
     gpio_config(&io_conf);
-/*
-    //interrupt of rising edge
-    io_conf.intr_type = GPIO_INTR_POSEDGE;
-    //bit mask of the pins, use GPIO4/5 here
-    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
-    //set as input mode
-    io_conf.mode = GPIO_MODE_INPUT;
-    //enable pull-up mode
-    io_conf.pull_up_en = 1;
-    gpio_config(&io_conf);
-*/
-    //change gpio intrrupt type for one pin
-//    gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
 
-    //create a queue to handle gpio event from isr
-   // gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
-    //start gpio task
-   // xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
-
-    //install gpio isr service
-   // gpio_install_isr_service(0);
-    //hook isr handler for specific gpio pin
-  //  gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void *) GPIO_INPUT_IO_0);
-    //hook isr handler for specific gpio pin
-  //  gpio_isr_handler_add(GPIO_INPUT_IO_1, gpio_isr_handler, (void *) GPIO_INPUT_IO_1);
-
-    //remove isr handler for gpio number.
-  //  gpio_isr_handler_remove(GPIO_INPUT_IO_0);
-    //hook isr handler for specific gpio pin again
-  //  gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void *) GPIO_INPUT_IO_0);
 
     int cnt = 0;
 
